bf7f1151f68a1487cea526bdb43b52ce85fdb30e
Braingenius2 <braingenius2@gmail.com>
2023-06-09 22:06:00 +0300

    Fix eslint errors automatically

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const Button = (props) => {
-  return (
-    <button className={props.className} onClick={props.onClick}>
-      {props.label}
-    </button>
-  );
-};
+const Button = (props) => (
+  <button className={props.className} onClick={props.onClick}>
+    {props.label}
+  </button>
+);
 
 export default Button;
