898bb31f11083f09deb681af8fba8a6e0f6a75fb
Braingenius2 <braingenius2@gmail.com>
2023-06-09 22:46:28 +0300

    Fix eslint errors. Run 'npm i -S prop-types'

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Button = ({ className, onClick, label }) => (
-  <button type="button" className={ className } onClick={ onClick }>
+  <button type="button" className={className} onClick={onClick}>
     { label }
   </button>
 );
+Button.propType = {
+  className: PropTypes.string,
+  onClick: PropTypes.func,
+  label: PropTypes.func,
+};
 
 export default Button;
