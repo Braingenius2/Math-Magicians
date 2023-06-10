ef2e7b58bb4a4f31453f2e340ef2f92dcc0f9034
Braingenius2 <braingenius2@gmail.com>
2023-06-10 04:27:46 +0300

    Fix module imports error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Calculator from './components/Calculator';
-import '../css/App.css';
+import './css/App.css';
 
 const App = () => (
   <section id="calculator-container">
