687a9cdd89179f9e6b6be3bde005ecb5eb1d7587
Braingenius2 <braingenius2@gmail.com>
2023-06-22 21:29:09 +0300

    Add Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  <div>
+    <h1>Math Magicians</h1>
+    <h2>Welcome to our page!</h2>
+    <p>Math magicians is a website for all fans of mathematics. It is a project that brings together the magic of mathematics and the magic of coding.</p>
+    <p>Here, we believe that "Mathematics is not about numbers, equations, computations, or algorithms: it is about understanding". Join us now and be a part of the magical world of mathematics.</p>
+  </div>
+};
+
+export default Home;
