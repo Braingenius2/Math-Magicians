2883418aaeda98f9edc71bd1bf60e3cf26e964eb
Braingenius2 <braingenius2@gmail.com>
2023-06-23 12:08:43 +0300

    Refactor styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 const Home = () => (
   <div>
-    <h2>Welcome to our page!</h2>
+    <h2 style={{ marginBottom: '2.5rem' }}>Welcome to our page!</h2>
     <p>
       Math magicians is a website for all fans of mathematics.
       It is a project that brings together the magic of mathematics and the magic of coding.
     </p>
+    <br />
     <p>
       Here, we believe that &quot;Mathematics is not about numbers,
       equations, computations, or algorithms: it is about understanding&quot;.
