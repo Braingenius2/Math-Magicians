8988ec2b26daf896408a47b64a7f543e64051660
Braingenius2 <braingenius2@gmail.com>
2023-06-23 11:22:31 +0300

    Update styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../css/Navbar.css';
 
 const Navbar = () => (
-  <nav>
-    <ul>
-      <li>
+  <nav className="navbar">
+    <h1 className="logo">Math Magicians</h1>
+    <ul className="nav-menu">
+      <li className="nav-link">
         <Link to="/">Home</Link>
       </li>
-      <li>
+      <li className="nav-link">
         <Link to="/calculator">Calculator</Link>
       </li>
-      <li>
+      <li className="nav-link">
         <Link to="/quote">Quote</Link>
       </li>
     </ul>
