7f544ec4c0bcb091b5b4ae81ff0d009f0cdd2a6c
Braingenius2 <braingenius2@gmail.com>
2023-06-24 14:21:40 +0300

    Add tests for Calculator.js component

---
+++ src/components/__tests__/Calculator.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Calculator from './Calculator';
+
+describe('Calculator', () => {
+  it('should render the calculator', () => {
+    const { getByText } = render(<Calculator />);
+    const heading = getByText("Let's do some math!");
+    expect(heading).toBeInTheDocument();
+  });
+
+  it('should update the display when a button is clicked', () => {
+    const { getByText, getByDisplayValue } = render(<Calculator />);
+    const button = getByText('7');
+    fireEvent.click(button);
+    const display = getByDisplayValue('7');
+    expect(display).toBeInTheDocument();
+  });
+});
